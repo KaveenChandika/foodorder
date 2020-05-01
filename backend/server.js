9d7fb81ac200fc49cadfb8cc2bae698abef3e827
KaveenChandika <ckaveen17@gmail.com>
2020-05-01 21:29:25 +0530

    login service and backend changes

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ var mysql = require('mysql');
 const bodyParser = require('body-parser');
 const mongo = require('mongodb');
 const bcrypt = require('bcrypt');
+const md5 = require('md5');
 const app = express();
 app.use(express.urlencoded());
 app.use(express.json());
@@ -17,44 +18,56 @@ var con = mysql.createConnection({
   database: "foodorder"
 });
 
+const login = [];
+
 app.get('/',(req,res)=>{
   res.send("Starting...")
 })
 
+app.post('/api/login', (req,res)=>{
+    let hash = md5(req.body.password);
+    const userLogin = {
+      username:req.body.username,
+      password:hash
+    }
+    const response = { result:true}
+    const error = {result:false}
+    var sqlLogin = `SELECT  * FROM tbl_user WHERE username='${userLogin.username}' and password='${userLogin.password}'`
+    con.query(sqlLogin,function(err,result){
+      if(err) throw err;
+      if(result.length == 0)
+        res.send({"result":false})
+      else
+        res.send({"result":true})
+    })
+})
+
+
 app.post('/api/addUsers',(req,res) =>{
-  let hash = bcrypt.hashSync(`${req.body.userData.pwd}`, 10);
-  con.connect(function(err){
-    if (err) throw err;
+    let hash = md5(req.body.userData.pwd);
     var sql = `INSERT INTO  tbl_user (fname,address,email,nic,tel,username,password,status)  VALUES ('${req.body.userData.name}', '${req.body.userData.address}', '${req.body.userData.email}', '${req.body.userData.nic}', '${req.body.userData.tel}', '${req.body.userData.uname}', '${hash}','0' ) `;
     con.query(sql,function(err,result){
-      if (err) throw err;
       console.log("1 record added");
-
     })
-  });
 })
 
 app.get('/api/viewUsers',(req,res)=>{
-  con.connect(function(err){
     var sql = "SELECT * FROM tbl_user tu WHERE tu.status=0";
     con.query(sql,function(err,result){
       res.send(result)
     })
-  });
 })
 
 app.get('/api/getEditUser/:id',(req,res)=>{
    var id = (req.params.id);
-  con.connect(function(err){
     var sql = `SELECT * FROM tbl_user tu WHERE tu.status=0 AND tu.u_id='${id}'`;
     con.query(sql,function(err,result){
       res.send(result);
     })
-  });
 })
 
 app.post('/api/updateUser',(req,res)=>{
-  let hash = bcrypt.hashSync(`${req.body.data.pwd}`, 10);
+  let hash = md5(req.body.data.pwd);
   var updateObj = {
     u_id:req.body.data.u_id,
     fname:req.body.data.name,
@@ -65,7 +78,6 @@ app.post('/api/updateUser',(req,res)=>{
     username:req.body.data.uname,
     password: hash   
   }
-  con.connect(function(err){
     var updateSql = `UPDATE tbl_user SET 
                         fname='${updateObj.fname}',
                         address='${updateObj.address}',
@@ -80,7 +92,6 @@ app.post('/api/updateUser',(req,res)=>{
       if (err) throw err;
       console.log("1 record updated");
     })
-  })
 })
 
 
